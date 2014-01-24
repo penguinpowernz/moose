6ef497129d3d00dc5dd39e43f7f86bf79a3beac1
Robert McLeod <robert@autogrow.com>
2014-01-25 04:24:19 +1300

    farmed out title parsing, made moose better
    - added markdown link building
    - added unreachable link saving
    - made moose rescue all errors (zomg!)

--- moose.rb
+++ moose.rb
@@ -3,29 +3,44 @@
 require 'json'
 require 'nokogiri'
 require 'open-uri'
+require './moose/title_parser'
 
 URL = "http://www.reddit.com/r/moosearchive/comments/1hhjnb/archive/"
+MD_FILE = "./moose.md"
+JSON_FILE = "./moose.json"
 
 puts "Getting comment links from: #{URL}"
 
 doc = Nokogiri::HTML(open(URL))
+nodes = []
+File.open("unreachable.txt","w") {|f| f.write "" }
 
 puts "#{doc.css(".usertext-body a").count} links" 
 
-nodes = Array.new
-
 doc.css(".usertext-body a").each do |link|
   begin
-    article = Nokogiri::HTML(open(link.content))
-    node = OpenStruct.new({})
-    node.title = article.css("title").first.content
-    node.href = link.content
-    nodes << node
-  rescue InvalidURIError
+    title = Moose::TitleParser.parse(link.content)
+
+    nodes << {
+      :title => title,
+      :href  => link.content
+    }
+
+    unless File.read(MD_FILE).include? link.content
+      File.open(MD_FILE, "a") do |f|
+        f.puts "[#{title}](#{link.content})"
+      end
+    end
+
+    puts "Added: #{link.content}"
+  rescue => e
+    STDERR.puts "Failed: #{link.content} (#{e.class.name}: #{e.message})"
+    File.open("unreachable.txt","a") {|f| f.puts "#{link.content};#{e.class.name};#{e.message}" }
     next
   end
 end
 
-File.open("moose.json", "w") {|f|
+File.open(JSON_FILE, "w") {|f|
   f.write nodes.to_json
 }
+
---
+++ moose/title_parser.rb
@@ -0,0 +1,16 @@
+module Moose
+  class TitleParser
+    def self.parse(link)
+      title = get_title_for link
+      title.gsub! /\s+/, " "
+      title.strip!
+      title
+    end
+
+    def self.get_title_for(link)
+      link = "http://#{link}" unless link.start_with? "http://"
+      body = Nokogiri::HTML(open(link))
+      body.css("head title").first.content
+    end
+  end
+end
